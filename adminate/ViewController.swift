9c35626a2a09b500f0a3f19ef2013e4713719283
Nick Coelius <ncoelius@gmail.com>
2020-08-18 11:11:48 -0700

    move files into right place, init xcode project

---
+++ adminate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  adminate
+//
+//  Created by Nick Coelius on 8/18/20.
+//  Copyright © 2020 Nick Coelius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
